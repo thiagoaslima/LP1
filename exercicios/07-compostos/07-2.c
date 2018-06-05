0c5430fc31df9131b8b88ffa005438e7fa8df68d
Thiago Lima <thiagoaslima@gmail.com>
2018-06-05 15:48:51 -0300

    preenche2 chamando preenche

--- exercicios/07-compostos/07-2.c
+++ exercicios/07-compostos/07-2.c
@@ -1,57 +1,141 @@
 #include <stdio.h>
+#include <string.h>
 
-int itens_counter = 0;
-struct ItemCardapio {
+enum tipoItemCardapio
+{
+    bebida = 1,
+    prato = 2,
+    sobremesa = 3
+};
+
+struct ItemCardapio
+{
     int id;
-    char nome[50];
+    char nome[30];
     float preco;
 };
 
-struct Mesa {
-    int qtdePedidos;
-    struct ItemCardapio* pedidos[100];
-    float totalConta;
+struct Cardapio
+{
+    int qtdePratos;
+    int qtdeBebidas;
+    int qtdeSobremesas;
+    int maxNome;
+    struct ItemCardapio pratos[20];
+    struct ItemCardapio bebidas[10];
+    struct ItemCardapio sobremesas[5];
 };
 
-void preenche(struct ItemCardapio* item) {
+void preenche(struct ItemCardapio *item)
+{
+    static int id = 0;
     char temp;
-    (*item).id = ++itens_counter;
 
-    printf("nome do prato: ");
-    scanf("%[^\n]", (*item).nome);
-
-    printf("valor do prato: ");
-    scanf("%f", &(*item).preco);
+    (*item).id = ++id;
 
     scanf("%c", &temp);
+
+    printf("nome: ");
+    scanf("%[^\n]", (*item).nome);
+
+    printf("valor: ");
+    scanf("%f", &(*item).preco);
 }
 
-void preenche2(struct Mesa* mesa, struct ItemCardapio* pedido) {
-    (*mesa).pedidos[(*mesa).qtdePedidos] = pedido;
-    (*mesa).qtdePedidos+=1;
-    (*mesa).totalConta += (*pedido).preco;
-}
+void preenche2(struct Cardapio *cardapio)
+{
+    int keep_going = 1;
+    int tipo;
+    int i;
 
-int main(void) {
-    struct ItemCardapio item1;
-    struct ItemCardapio item2;
+    struct ItemCardapio item;
 
-    preenche(&item1);
-    preenche(&item2);
+    (*cardapio).qtdePratos = 0;
+    (*cardapio).qtdeBebidas = 0;
+    (*cardapio).qtdeSobremesas = 0;
 
-    struct Mesa mesa;
-    mesa.qtdePedidos = 0;
-    mesa.totalConta = 0;
+    printf("Cadastre um item no cardápio: ");
 
-    preenche2(&mesa, &item1);
-    preenche2(&mesa, &item2);
+    while (keep_going)
+    {
 
-    printf("\nA mesa fez %d pedidos, totalizando R$ %.2f\n", mesa.qtdePedidos, mesa.totalConta);
-    printf("Os pedidos foram:\n");
+        printf("Tipo (1 = bebida, 2 = prato principal, 3 = sobremesa): ");
+        scanf("%d", &tipo);
 
-    for (int i = 0; i < mesa.qtdePedidos; i++) {
-        printf("\t%s\n", (*mesa.pedidos[i]).nome);
+        switch (tipo)
+        {
+        case bebida:
+            preenche(&(*cardapio).bebidas[(*cardapio).qtdeBebidas]);
+            item = (*cardapio).bebidas[(*cardapio).qtdeBebidas];
+            (*cardapio).qtdeBebidas += 1;
+            break;
+
+        case prato:
+            preenche(&(*cardapio).pratos[(*cardapio).qtdePratos]);
+            item = (*cardapio).pratos[(*cardapio).qtdePratos];
+            (*cardapio).qtdePratos++;
+            break;
+
+        case sobremesa:
+            preenche(&(*cardapio).sobremesas[(*cardapio).qtdeSobremesas]);
+            item = (*cardapio).sobremesas[(*cardapio).qtdeSobremesas];
+            (*cardapio).qtdeSobremesas++;
+            break;
+        }
+
+        if (strlen((item).nome) > (*cardapio).maxNome)
+        {
+            (*cardapio).maxNome = strlen((item).nome);
+        }
+
+        printf("Deseja cadastrar um novo item no cardápio (0 = não, 1 = sim)? ");
+        scanf("%d", &keep_going);
     }
+}
+
+void imprimirCardapio(struct Cardapio cardapio)
+{
+    if (cardapio.qtdePratos > 0)
+    {
+        printf("%s", "\n*** PRATOS ***\n");
+
+        for (int i = 0; i < cardapio.qtdePratos; i++)
+        {
+            printf("%*s", cardapio.maxNome, cardapio.pratos[i].nome);
+            printf(" : R$ %3.2f\n", cardapio.pratos[i].preco);
+        }
+    }
+
+    if (cardapio.qtdePratos > 0)
+    {
+        printf("%s", "\n*** BEBIDAS *** \n");
+
+        for (int i = 0; i < cardapio.qtdeBebidas; i++)
+        {
+            printf("%*s", cardapio.maxNome, cardapio.bebidas[i].nome);
+            printf(" : R$ %3.2f\n", cardapio.bebidas[i].preco);
+        }
+    }
+
+    if (cardapio.qtdePratos > 0)
+    {
+        printf("%s", "\n*** SOBREMESAS *** \n");
+
+        for (int i = 0; i < cardapio.qtdeSobremesas; i++)
+        {
+            printf("%*s", cardapio.maxNome, cardapio.sobremesas[i].nome);
+            printf(" : R$ %3.2f\n", cardapio.sobremesas[i].preco);
+        }
+    }
+}
+
+int main(void)
+{
+
+    struct Cardapio cardapio;
+
+    preenche2(&cardapio);
+    imprimirCardapio(cardapio);
 
     return 0;
 }
